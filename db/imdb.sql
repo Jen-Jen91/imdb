8ad4b0fddb2ec0525fa7cf9000a1998967daed05
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-09-27 14:29:09 +0100

    Added tables

---
+++ db/imdb.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE actors(
+  id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255)
+);
+
+CREATE TABLE movies(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255)
+);
+
+CREATE TABLE roles(
+  id SERIAL8 PRIMARY KEY,
+  actor_id INT8 REFERENCES actors(id) ON DELETE CASCADE,
+  movie_id INT8 REFERENCES movies(id) ON DELETE CASCADE,
+  fee INT8
+);
